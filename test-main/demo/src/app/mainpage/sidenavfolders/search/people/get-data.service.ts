ff6b73871b34e6d70bf8058b577f25a672c90750
testingV19 <140785235+testingV19@users.noreply.github.com>
2023-11-10 14:11:07 +0530

    Search is added

--- test-main/demo/src/app/mainpage/sidenavfolders/search/people/get-data.service.ts
+++ test-main/demo/src/app/mainpage/sidenavfolders/search/people/get-data.service.ts
@@ -1,20 +1,41 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
-  private apiUrl = 'http://localhost:8080'; 
+  private apiUrl = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getAllData(page: number, size: number, jobTitles: string[]) {
-    const params = new HttpParams()
+  getAllData(
+    page: number,
+    size: number,
+    jobTitles?: string[],
+    location?: string,
+    country?: string
+  ): Observable<any> {
+    let params = new HttpParams()
       .set('page', page.toString())
-      .set('size', size.toString())
-      .set('jobTitles', jobTitles.join(',')); // Convert array to comma-separated string
-
+      .set('size', size.toString());
+  
+    if (jobTitles) {
+      params = params.set('JobTitles', jobTitles.join(','));
+    }
+  
+    if (location) {
+      params = params.set('Location', location);
+    }
+  
+    if (country) {
+      params = params.set('Country', country);
+    }
+  
     return this.http.get(`${this.apiUrl}/api/all`, { params });
   }
+  getListOfCountries(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/all?Country`);
+  }  
 }
--- test-main/demo/src/app/mainpage/sidenavfolders/search/people/people.component.ts
+++ test-main/demo/src/app/mainpage/sidenavfolders/search/people/people.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GetDataService } from './get-data.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-people',
@@ -8,22 +9,39 @@ import { GetDataService } from './get-data.service';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
+  jobname = [1, 23, 34, 4, 5, 3];
   dataList: any;
+  allData: any;
   currentPage: number = 0;
-  pageSize: number = 10;
+  pageSize: number = 100;
   jobTitles: string[] = [];
+  locationFilter: string = '';
+  countries$: any=[];
+  countryFilter: any;
 
-  constructor(private route: ActivatedRoute, private router: Router, private service: GetDataService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private service: GetDataService) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.countries$ = this.service.getListOfCountries();
+
+    }, 500);
     this.loadData();
   }
 
   loadData() {
-    this.service.getAllData(this.currentPage, this.pageSize, this.jobTitles).subscribe((res: any) => {
-      console.log(res, "Get all data");
-      this.dataList = res; // Assuming your data is in the 'content' property
-    });
+    this.service
+      .getAllData(
+        this.currentPage,
+        this.pageSize,
+        this.jobTitles,
+        this.locationFilter,
+        this.countryFilter
+      )
+      .subscribe((res: any) => {
+        console.log(res, 'Get paginated data');
+        this.dataList = res;
+      });
   }
 
   isPeopleRoute(): boolean {
@@ -43,9 +61,11 @@ export class PeopleComponent implements OnInit {
     }
   }
 
+  
+
   onJobTitleChange(selectedJobTitles: string[]) {
     this.jobTitles = selectedJobTitles;
-    this.currentPage = 0; // Reset to first page when filter changes
+    this.currentPage = 0;
     this.loadData();
   }
 }
